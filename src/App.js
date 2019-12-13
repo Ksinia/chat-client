6b9f286fb6f743758d9d2dc597769feb825b9ab2
Ksinia <xiatian@mail.ru>
2019-12-13 15:53:45 +0100

    Move action creation to the server

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 
 class App extends Component {
   stream = new EventSource("http://localhost:4000/stream");
@@ -7,13 +8,39 @@ class App extends Component {
     // onmessage is a function that will run each time when the mew message comes
     this.stream.onmessage = event => {
       console.log("event test:", event.data);
-      const parsed = JSON.parse(event.data);
-      console.log(parsed);
+      const action = JSON.parse(event.data);
+      console.log(action);
+      this.props.dispatch(action);
     };
   }
   render() {
-    return <div>client</div>;
+    console.log("this.props.messages test", this.props.messages);
+
+    const { messages } = this.props;
+    const list = messages;
+    return (
+      <div>
+        {list.map(message => (
+          <p>{message.text}</p>
+        ))}
+      </div>
+    );
   }
 }
 
-export default App;
+// get data from store
+function mapStateToProps(state) {
+  // state is the current data in the redux store
+
+  //Each property of the object becomes a props of the component
+  return {
+    messages: state //this.props.messages will be the entire state of the redux store
+  };
+}
+
+// put data into the store
+// we don't need it anymore, because we moved action creation to the server side
+// const mapDispatchToProps = { allMessages }; // the action can be dispatched by running this.props.allMessages
+
+// connect is a thunk
+export default connect(mapStateToProps)(App);
--- src/actions.js
+++ src/actions.js
@@ -1,17 +1,19 @@
-export const ALL_MESSAGES = "ALL_MESSAGES";
+// now we don't need this file because we moved action creation to the server side
 
-export function allMessages(messages) {
-  return {
-    type: ALL_MESSAGES,
-    payload: messages
-  };
-}
+// export const ALL_MESSAGES = "ALL_MESSAGES";
 
-export const NEW_MESSAGE = "NEW_MESSAGE";
+// export function allMessages(messages) {
+//   return {
+//     type: ALL_MESSAGES,
+//     payload: messages
+//   };
+// }
 
-export function newMessaage(message) {
-  return {
-    type: NEW_MESSAGE,
-    payload: message
-  };
-}
+// export const NEW_MESSAGE = "NEW_MESSAGE";
+
+// export function newMessaage(message) {
+//   return {
+//     type: NEW_MESSAGE,
+//     payload: message
+//   };
+// }
